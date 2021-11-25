ccf9d2e8b84ef46f513f9111aeb12a2ae4b1c49a
paijojr <vpaijob@gmail.com>
2021-11-25 14:00:23 +0700

    Main Activity

--- app/src/main/java/paijojr/bfaasubmission1/MainActivity.kt
+++ app/src/main/java/paijojr/bfaasubmission1/MainActivity.kt
@@ -2,8 +2,15 @@ package paijojr.bfaasubmission1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import paijojr.bfaasubmission1.model.UserModel
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var recycler: RecyclerView
+    private val list = ArrayList<UserModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- app/src/main/java/paijojr/bfaasubmission1/adapter/UserAdapter.kt
+++ app/src/main/java/paijojr/bfaasubmission1/adapter/UserAdapter.kt
@@ -1,4 +1,56 @@
 package paijojr.bfaasubmission1.adapter
 
-class UserAdapter {
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import paijojr.bfaasubmission1.R
+import paijojr.bfaasubmission1.model.UserModel
+
+class UserAdapter(private val listUser: ArrayList<UserModel>) : RecyclerView.Adapter<UserAdapter.ListViewHolder>() {
+    private lateinit var onItemClickCallback: OnItemClickCallback
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
+    interface OnItemClickCallback {
+        fun onItemClicked(data: UserModel)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_user, parent, false)
+        return ListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val contexts: Context = holder.itemView.context
+        val imageResource: Int = contexts.resources.getIdentifier(listUser[position].avatar, null, contexts.packageName)
+        val res: Drawable? = ResourcesCompat.getDrawable(contexts.resources, imageResource, null)
+
+        Glide.with(contexts)
+            .load(res)
+            .circleCrop()
+            .into(holder.imageAvatar)
+        holder.textLocation.text = listUser[position].location
+        holder.textName.text = listUser[position].name
+
+        holder.imageAvatar.setOnClickListener{ onItemClickCallback.onItemClicked(listUser[holder.adapterPosition]) }
+    }
+
+    override fun getItemCount(): Int = listUser.size
+
+    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var imageAvatar: ImageView = itemView.findViewById(R.id.item_avatar)
+        var textName: TextView = itemView.findViewById(R.id.item_text_name)
+        var textLocation: TextView = itemView.findViewById(R.id.item_text_location)
+
+    }
+
 }
